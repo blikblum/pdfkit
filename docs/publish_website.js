d257ec6a47afc0bb849d33f60018aa53d9d49acd
Luiz Americo <blikblum@users.noreply.github.com>
2026-08-30 20:38:46 -0300

    Improve publish_website script

--- docs/publish_website.js
+++ docs/publish_website.js
@@ -2,7 +2,7 @@ const ghpages = require('gh-pages');
 
 const { argv } = process;
 
-const message = argv.length > 2 ? argv[2] : undefined;
+const message = argv.length > 2 ? argv[2] : 'Update gh-pages branch';
 
 ghpages.publish(
   '.',
@@ -18,12 +18,17 @@ ghpages.publish(
       'examples/browserify/bundle.js',
       'examples/kitchen-sink.pdf',
     ],
+    repo: 'https://github.com/foliojs/pdfkit.git',
     add: true,
     message,
   },
   function (err) {
     if (err) {
       console.error(err);
+      process.exitCode = 1;
+      return;
     }
+
+    console.log('Website published to the gh-pages branch.');
   },
 );
